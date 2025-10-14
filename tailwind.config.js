dc4317ebf3c8fc7ae8d7ba3b49384a1812645990
rebelliousi <learneryolo3@gmail.com>
2025-10-14 15:10:59 +0500

    index css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,104 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "app/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "darker-gray": "var(--darker-gray)",
+        "dark-blue-gray": "var(--dark-blue-gray)",
+        "light-gray": "var(--light-gray)",
+        "lighter-gray": "var(--lighter-gray)",
+        "medium-new-gray": "var(--medium-new-gray)",
+        "medium-blue-gray":
+          "var(--medium-blue-gray)",
+        "gray-white": "var(--gray-white)",
+        "primary": "var(--primary)",
+        "primary-v2": "var(--primary-v2)",
+        "lightest-blue": "var(--lightest-blue)",
+        "lightest-blue-3": "var(--lightest-blue-3)",
+        "lightest-gray": "var(--lightest-gray)",
+        "main-text": "var(--main-text)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      fontFamily: {
+        "regular": "var(--regular-font-family)",
+        "big": "var(--big-font-family)",
+        "h3-semib":
+          "var(--h3-semib-font-family)",
+        "h5-semib":
+          "var(--h5-semib-font-family)",
+        "h6-semib":
+          "var(--h6-semib-font-family)",
+        "normal": "var(--font-family)",
+        "normal-semibold":
+          "var(--semibold-font-family)",
+        sans: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+    container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
   },
   plugins: [],
-}
-
+  darkMode: ["class"],
+};
